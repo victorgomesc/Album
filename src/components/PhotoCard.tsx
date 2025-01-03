c06a5644b522b3604ef7e8c5389db4314ba1fb9e
Victor Gomes <victorgomesdacosta18@gmail.com>
2025-01-03 11:57:32 -0300

    feat:implemeted photocard

--- src/components/PhotoCard.tsx
+++ src/components/PhotoCard.tsx
@@ -1,9 +1,10 @@
+import Image from 'next/image'
 import React from 'react'
 
 const PhotoCard = () => {
   return (
-    <div>
-      
+    <div className="p-4 border-4 border-gray-800 max-w-fit rounded-xl shadow-lg bg-black">
+      <Image src="/assets/foto1.jpeg" alt='foto1' width={400} height={550} className="rounded-lg max-w-full h-auto" />
     </div>
   )
 }

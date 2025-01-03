d90a117deb3683d6a133adfe74cb9453a350daac
Victor Gomes <victorgomesdacosta18@gmail.com>
2025-01-02 22:31:08 -0300

    style: backgroun image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Header from "@/components/Header";
 
 export default function Home() {
   return (
-    <div>
+    <div className="w-full h-full bg-[#191a1a] bg-grid-pattern bg-55">
       <Header />
     </div>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,18 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        'custom-gray': 'rgba(114, 114, 114, 0.3)',
+        'foreground': 'rgb(var(--foreground))',
+        'background': 'rgb(var(--background))',
+      },
+      backgroundImage: {
+        'grid-pattern': `
+          linear-gradient(0deg, transparent 24%, rgba(114, 114, 114, 0.3) 25%, rgba(114, 114, 114, 0.3) 26%, transparent 27%, transparent 74%, rgba(114, 114, 114, 0.3) 75%, rgba(114, 114, 114, 0.3) 76%, transparent 77%, transparent),
+          linear-gradient(90deg, transparent 24%, rgba(114, 114, 114, 0.3) 25%, rgba(114, 114, 114, 0.3) 26%, transparent 27%, transparent 74%, rgba(114, 114, 114, 0.3) 75%, rgba(114, 114, 114, 0.3) 76%, transparent 77%, transparent)
+        `,
+      },
+      backgroundSize: {
+        '55': '55px 55px',
       },
     },
   },

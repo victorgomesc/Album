8627f22d1c27a77fde32e49c78587b42f4ba3200
Victor Gomes <victorgomesdacosta18@gmail.com>
2025-01-03 19:07:58 -0300

    feat: create componete footer

--- src/components/PhotoCard.tsx
+++ src/components/PhotoCard.tsx
@@ -5,6 +5,11 @@ const PhotoCard = () => {
   return (
     <div className="p-4 border-4 border-gray-800 max-w-fit rounded-xl shadow-lg bg-black">
       <Image src="/assets/foto1.jpeg" alt='foto1' width={400} height={550} className="rounded-lg max-w-full h-auto" />
+      <div className="flex">
+        <h2>titulo</h2>
+        <span>17/07/2024</span>
+      </div>
+      
     </div>
   )
 }

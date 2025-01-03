5a5ffa21484bd4a95afa8ccd24a68bc25bc0b890
Victor Gomes <victorgomesdacosta18@gmail.com>
2025-01-03 20:33:26 -0300

    style: innstall styled components and implement PhotoCard

--- src/components/PhotoCard.tsx
+++ src/components/PhotoCard.tsx
@@ -1,17 +1,51 @@
-import Image from 'next/image'
 import React from 'react'
+import Image from 'next/image';
+import StyledWrapper from './StyledWrapper';
 
 const PhotoCard = () => {
   return (
-    <div className="p-4 border-4 border-gray-800 max-w-fit rounded-xl shadow-lg bg-black">
-      <Image src="/assets/foto1.jpeg" alt='foto1' width={400} height={550} className="rounded-lg max-w-full h-auto" />
-      <div className="flex">
-        <h2>titulo</h2>
-        <span>17/07/2024</span>
+    <StyledWrapper>
+      <div className="card">
+        <div className="content">
+          <div className="back">
+            <div className="back-content">
+              <Image 
+                src="/assets/foto1.jpeg" 
+                alt='foto1' 
+                width={600} 
+                height={600} 
+                className='flex p-8 items-center justify-center' 
+              />
+              <strong>Hover Me</strong>
+            </div>
+          </div>
+          <div className="front">
+            <div className="img">
+              <div className="circle">
+              </div>
+              <div className="circle" id="right">
+              </div>
+              <div className="circle" id="bottom">
+              </div>
+            </div>
+            <div className="front-content">
+              <small className="badge">Pasta</small>
+              <div className="description">
+                <div className="title">
+                  <p className="title">
+                    <strong>Spaguetti Bolognese</strong>
+                  </p>
+                </div>
+                <p className="card-footer">
+                  30 Mins &nbsp; | &nbsp; 1 Serving
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      
-    </div>
-  )
+    </StyledWrapper>
+  );
 }
 
 export default PhotoCard

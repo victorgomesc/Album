ef7064c2a5639efe078e2e7eb2818d0b80a23f49
Victor Gomes <victorgomesdacosta18@gmail.com>
2025-01-04 22:36:46 -0300

    feat: implement a new card

--- src/components/PhotoCard.tsx
+++ src/components/PhotoCard.tsx
@@ -103,6 +103,54 @@ const PhotoCard = () => {
         </div>
       </div>
     </StyledWrapper>
+
+    <StyledWrapper>
+      <div className="card">
+        <div className="content">
+          <div className="back">
+            <div className="back-content">
+              <Image 
+                src="/assets/foto2.jpg" 
+                alt='foto1' 
+                width={600} 
+                height={400} 
+                className='flex object-cover p-8 items-center w-full h-5/6 justify-center' 
+              />
+              <strong className='flex gap-8'>
+                <CgPlayTrackPrev size={42} />
+                <IoMdPlayCircle size={42} />
+                <MdSkipNext size={42} />
+              </strong>
+            </div>
+          </div>
+          <div className="front">
+            <div className="img">
+              <div className="circle">
+              </div>
+              <div className="circle" id="right">
+              </div>
+              <div className="circle" id="bottom">
+              </div>
+            </div>
+            <div className="front-content">
+              <small className="badge">Dia no shoping</small>
+              <div className="description">
+                <div className="title">
+                  <p className="title">
+                    <strong>
+                      dia no shoping
+                    </strong>
+                  </p>
+                </div>
+                <p className="card-footer">
+                  30 Mins &nbsp; | &nbsp; 1 Serving
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </StyledWrapper>
     </div>
   );
 }
